67cf752bf5de835c4110752a1f0806f2c8e8a46d
noavrd <noavrd@gmail.com>
2021-12-03 01:57:51 +0200

    add grid css

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Main from './components/Main';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Calculator</header>
+      <header className="App-header">
+        <h1>Calculator</h1>
+      </header>
       <Main />
     </div>
   );
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,19 +2,26 @@ import Action from './Action';
 import Number from './Number';
 export default function Main() {
   const symbols = ['/', '*', '-', '+'];
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
   return (
     <>
-      {symbols.map((element, i) => (
-        <div className="symbols">
-          <Action symbol={element} key={i} />
-        </div>
-      ))}
-      {numbers.map((element, i) => (
-        <div className="numbers">
-          <Number eachNum={element} key={i} />
-        </div>
-      ))}
+      <div className="show-clicked"></div>
+      <div className="symbols-container">
+        {symbols.map((element, i) => (
+          <div className="symbol">
+            <Action symbol={element} key={i} />
+          </div>
+        ))}
+      </div>
+
+      <div className="numbers-container">
+        {numbers.map((element, i) => (
+          <div className="number">
+            <Number eachNum={element} key={i} />
+          </div>
+        ))}
+      </div>
+
       <div>Clear</div>
     </>
   );
